f2a49dbdc1e2c25d5124b70eed10682206642ab4
Chin Guang <ganchinguang0622@gmail.com>
2026-06-24 13:26:34 +0800

    Integrate with Retell

--- backend/apis/twilio_api.py
+++ backend/apis/twilio_api.py
@@ -1,12 +1,24 @@
-from twilio.twiml.voice_response import VoiceResponse, Connect
+from twilio.twiml.voice_response import VoiceResponse, Connect, Dial
 
+from environment import RETELL_AGENT_ID, TWILIO_PHONE_NUMBER
 from main import app
 from fastapi import Request, Response
+
+from retell_module import retell_client
 
 
 @app.post("/voice")
 async def twilio_webhook(req: Request, res: Response):
+    phone_call_response = retell_client.call.register_phone_call(
+        agent_id=RETELL_AGENT_ID,
+        from_number=req.body()["From"],
+        to_number=TWILIO_PHONE_NUMBER,
+        direction="inbound"
+    )
     response = VoiceResponse()
+    dial = Dial()
+    dial.sip(f"sip:{phone_call_response.call_id}@sip.retellai.com")
+    response.append(dial)
     connect = Connect()
     connect.stream(url=f"wss://{req.headers.get("host")}/media-stream")
     response.append(connect)
---
+++ backend/retell_module.py
@@ -0,0 +1,8 @@
+from retell import Retell
+
+from environment import RETELL_API_KEY
+
+retell_client = Retell(
+    api_key=RETELL_API_KEY
+)
+
